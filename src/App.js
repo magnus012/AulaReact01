e2cfeb7ffcf0993f72cfe3694524ded967f4fb2c
magnus012 <igor_012@hotmail.com>
2024-08-19 10:15:35 -0300

    Aula03

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Data from "./meusComponentes/Data.js"
 function App() {
   return (
     <div className="App">
-      <Data texto="Data e hora atuais"/>
-      
+      <Data texto="Data e hora atuais" timeZone="-4.00"/>
+      <Data texto="Data e hora atuais" timeZone="-2.00"/>
     </div>
   );
 }
