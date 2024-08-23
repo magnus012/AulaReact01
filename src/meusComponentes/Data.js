e49cdbc961c3afe5e9b007f83588cb89a8aa9d36
magnus012 <igor_012@hotmail.com>
2024-08-23 08:57:50 -0300

    console log removidos

--- src/meusComponentes/Data.js
+++ src/meusComponentes/Data.js
@@ -20,7 +20,6 @@ export default class Data extends Component{
     }
     //fase de montagem
     componentDidMount(){
-        console.log("O componente foi montado.");
         //Não é permitido atualizar o estado do componente de forma direta
         //this.state = ...
         this.setState({
@@ -29,7 +28,6 @@ export default class Data extends Component{
     }
 
     componentDidUpdate(){
-        console.log("O componente foi atualizado!");
         setTimeout(()=>{
             this.setState({
                 dataAtual: this.pegaDataDe(this.props.timeZone).toLocaleString()
